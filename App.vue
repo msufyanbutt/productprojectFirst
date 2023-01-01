1c61aabd11ae5e103abdfbddc144eb41e486acda
msufyanbutt <121723946+msufyanbutt@users.noreply.github.com>
2023-01-01 22:37:56 +0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="w-full h-screen bg-yellow-100 items-center fixed" >
+        <div class="sm:flex flex-row w-96 h-80  m-auto mt-32 ">
+            
+          <div class=" w-48 h-full ">
+                <img src="./assets/product2.jpg" class="min-h-full rounded-lg">
+            </div>
+            
+            <div class="w-48 h-full bg-white">
+                <p class="uppercase text-gray-500 font-mono px-2 py-2">p e r f u m e</p>
+                <h1 class="text-3xl font-bold px-2 py-2">Gabrielle Essence Eau De Parfume</h1>
+
+                <p class="px-2 py-2 text-xs text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, consequuntur odit? Quaerat</p>
+
+                <span class="text-2xl text-green-500 px-2 py-2 font-bold">$149.99</span>
+              <div class="flex flex-row bg-green-900 text-white px-10  py-2 text-xs my-2 mx-2 rounded-sm cursor-pointer">
+                <span class="mdi mdi-cart-minus "></span>
+                <p class="px-2">Add to Cart</p>
+              </div>
+                
+                
+
+            </div>
+          </div>
+            
+        </div>
+
+    </div>
+    
+</template>
+
